dcc8cb0362a33f6f8811eb220c5256f4791b0535
SamirAhmad5445 <samirahmad5445@gmail.com>
2023-02-08 17:48:13 +0200

    change the DOM manipulation

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 const card = document.querySelector(".card");
+const cardImage = document.querySelector(".card img");
 const gridDimension = 16;
 
 for (let i = 0; i < gridDimension ** 2; i++) {
   let mouseTracker = document.createElement("div");
   mouseTracker.className = "mouse-tracker";
-  card.appendChild(mouseTracker);
+  card.insertBefore(mouseTracker, cardImage);
   // console.log(i);
 }
-
-const cardImage = document.createElement("img");
-cardImage.setAttribute("src", card.getAttribute("data-src"));
-cardImage.setAttribute("alt", card.getAttribute("data-alt"));
-cardImage.className = "card-image";
-
-card.appendChild(cardImage);
